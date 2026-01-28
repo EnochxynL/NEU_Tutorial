6a4dd74ed26daa20550efd07345f51c10200f42f
enoch <enochxyn@outlook.com>
2026-01-28 12:06:46 +0800

    能运行了

--- blazepalm.py
+++ blazepalm.py
@@ -347,6 +347,57 @@ class BlazeDetector(BlazeBase):
         mediapipe/calculators/util/non_max_suppression_calculator.cc
         mediapipe/calculators/util/non_max_suppression_calculator.proto
         """
+        
+        # IOU code from https://github.com/amdegroot/ssd.pytorch/blob/master/layers/box_utils.py
+
+        def intersect(box_a, box_b):
+            """ We resize both tensors to [A,B,2] without new malloc:
+            [A,2] -> [A,1,2] -> [A,B,2]
+            [B,2] -> [1,B,2] -> [A,B,2]
+            Then we compute the area of intersect between box_a and box_b.
+            Args:
+            box_a: (tensor) bounding boxes, Shape: [A,4].
+            box_b: (tensor) bounding boxes, Shape: [B,4].
+            Return:
+            (tensor) intersection area, Shape: [A,B].
+            """
+            A = box_a.size(0)
+            B = box_b.size(0)
+            max_xy = torch.min(box_a[:, 2:].unsqueeze(1).expand(A, B, 2),
+                            box_b[:, 2:].unsqueeze(0).expand(A, B, 2))
+            min_xy = torch.max(box_a[:, :2].unsqueeze(1).expand(A, B, 2),
+                            box_b[:, :2].unsqueeze(0).expand(A, B, 2))
+            inter = torch.clamp((max_xy - min_xy), min=0)
+            return inter[:, :, 0] * inter[:, :, 1]
+
+
+        def jaccard(box_a, box_b):
+            """Compute the jaccard overlap of two sets of boxes.  The jaccard overlap
+            is simply the intersection over union of two boxes.  Here we operate on
+            ground truth boxes and default boxes.
+            E.g.:
+                A ∩ B / A ∪ B = A ∩ B / (area(A) + area(B) - A ∩ B)
+            Args:
+                box_a: (tensor) Ground truth bounding boxes, Shape: [num_objects,4]
+                box_b: (tensor) Prior boxes from priorbox layers, Shape: [num_priors,4]
+            Return:
+                jaccard overlap: (tensor) Shape: [box_a.size(0), box_b.size(0)]
+            """
+            inter = intersect(box_a, box_b)
+            area_a = ((box_a[:, 2]-box_a[:, 0]) *
+                    (box_a[:, 3]-box_a[:, 1])).unsqueeze(1).expand_as(inter)  # [A,B]
+            area_b = ((box_b[:, 2]-box_b[:, 0]) *
+                    (box_b[:, 3]-box_b[:, 1])).unsqueeze(0).expand_as(inter)  # [A,B]
+            union = area_a + area_b - inter
+            return inter / union  # [A,B]
+
+
+        def overlap_similarity(box, other_boxes):
+            """Computes the IOU between a bounding box and set of other boxes."""
+            return jaccard(box.unsqueeze(0), other_boxes).squeeze(0)
+
+
+
         if len(detections) == 0: return []
 
         output_detections = []
@@ -905,6 +956,7 @@ class BlazeRunner:
             palm_detections = self.hand_detection(frame)
             rect_detected = self.detection_to_rectangle(palm_detections)
         else:
+            palm_detections = None
             rect_detected = self.rect_saved
         xc, yc, scale, theta = rect_detected
 
@@ -1044,7 +1096,8 @@ if __name__ == "__main__":
         xc, yc, theta, scale = runnner_debug['roi']
 
         rendered_frame = frame.copy()
-        Main.draw_detections(rendered_frame, palm_detections)
+        if palm_detections is not None:
+            Main.draw_detections(rendered_frame, palm_detections)
         Main.draw_annotation(rendered_frame, landmarks, flags)
         Main.draw_roi(rendered_frame, xc, yc, theta, scale)
         cv2.imshow(WINDOW, rendered_frame[:,:,::-1])
