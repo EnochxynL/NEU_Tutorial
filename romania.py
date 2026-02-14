def6cf32089535abbfa890350a64dcd12028098e
enoch <enochxyn@outlook.com>
2026-02-14 10:15:48 +0800

    UCS

--- romania.py
+++ romania.py
@@ -3,72 +3,100 @@
 import queue
 import matplotlib.pyplot as plt
 
-# getting heuristics from file
-def getHeuristics():
-    heuristics = {}
-    f = open("heuristics.txt")
-    for i in f.readlines():
-        node_heuristic_val = i.split()
-        heuristics[node_heuristic_val[0]] = int(node_heuristic_val[1])
+class DataLoader:
+    DATA_PATH = "assets/romania/"
 
-    return heuristics
+    @classmethod
+    def getHeuristics(cls):
+        ''' getting heuristics from file'''
+        heuristics = {}
+        f = open(cls.DATA_PATH+"heuristics.txt")
+        for i in f.readlines():
+            node_heuristic_val = i.split()
+            heuristics[node_heuristic_val[0]] = int(node_heuristic_val[1])
+
+        return heuristics
+    
+    @classmethod
+    def getCity(cls):
+        ''' getting cities location from file'''
+        city = {}
+        citiesCode = {}
+        f = open(cls.DATA_PATH+"cities.txt")
+        j = 1
+        for i in f.readlines():
+            node_city_val = i.split()
+            city[node_city_val[0]] = [int(node_city_val[1]), int(node_city_val[2])]
+
+            citiesCode[j] = node_city_val[0]
+            j += 1
+
+        return city, citiesCode
+    
+    @classmethod
+    def createGraph(cls):
+        ''' creating cities graph from file'''
+        graph = {}
+        file = open(cls.DATA_PATH+"citiesGraph.txt")
+        for i in file.readlines():
+            node_val = i.split()
+
+            if node_val[0] in graph and node_val[1] in graph:
+                c = graph.get(node_val[0])
+                c.append([node_val[1], node_val[2]])
+                graph.update({node_val[0]: c})
+
+                c = graph.get(node_val[1])
+                c.append([node_val[0], node_val[2]])
+                graph.update({node_val[1]: c})
+
+            elif node_val[0] in graph:
+                c = graph.get(node_val[0])
+                c.append([node_val[1], node_val[2]])
+                graph.update({node_val[0]: c})
+
+                graph[node_val[1]] = [[node_val[0], node_val[2]]]
+
+            elif node_val[1] in graph:
+                c = graph.get(node_val[1])
+                c.append([node_val[0], node_val[2]])
+                graph.update({node_val[1]: c})
+
+                graph[node_val[0]] = [[node_val[1], node_val[2]]]
+
+            else:
+                graph[node_val[0]] = [[node_val[1], node_val[2]]]
+                graph[node_val[1]] = [[node_val[0], node_val[2]]]
+
+        return graph
 
 
-# getting cities location from file
-def getCity():
-    city = {}
-    citiesCode = {}
-    f = open("cities.txt")
-    j = 1
-    for i in f.readlines():
-        node_city_val = i.split()
-        city[node_city_val[0]] = [int(node_city_val[1]), int(node_city_val[2])]
+def UCS(startNode, graph, goalNode="Bucharest"):
+    ''' Uniform Cost Search Algorithm '''
+    priorityQueue = queue.PriorityQueue()
+    distance = 0
+    priorityQueue.put((distance, [startNode, 0]))
 
-        citiesCode[j] = node_city_val[0]
-        j += 1
+    path = []
 
-    return city, citiesCode
+    while priorityQueue.empty() == False:
+        current = priorityQueue.get()[1]
+        path.append(current[0])
+        distance += int(current[1])
+        
+        if current[0] == goalNode:
+            break
 
+        priorityQueue = queue.PriorityQueue()
+        
+        for i in graph[current[0]]:
+            if i[0] not in path:
+                priorityQueue.put((int(i[1]) + distance, i))
+        
+    return path
 
-# creating cities graph from file
-def createGraph():
-    graph = {}
-    file = open("citiesGraph.txt")
-    for i in file.readlines():
-        node_val = i.split()
-
-        if node_val[0] in graph and node_val[1] in graph:
-            c = graph.get(node_val[0])
-            c.append([node_val[1], node_val[2]])
-            graph.update({node_val[0]: c})
-
-            c = graph.get(node_val[1])
-            c.append([node_val[0], node_val[2]])
-            graph.update({node_val[1]: c})
-
-        elif node_val[0] in graph:
-            c = graph.get(node_val[0])
-            c.append([node_val[1], node_val[2]])
-            graph.update({node_val[0]: c})
-
-            graph[node_val[1]] = [[node_val[0], node_val[2]]]
-
-        elif node_val[1] in graph:
-            c = graph.get(node_val[1])
-            c.append([node_val[0], node_val[2]])
-            graph.update({node_val[1]: c})
-
-            graph[node_val[0]] = [[node_val[1], node_val[2]]]
-
-        else:
-            graph[node_val[0]] = [[node_val[1], node_val[2]]]
-            graph[node_val[1]] = [[node_val[0], node_val[2]]]
-
-    return graph
-
-
-# Greedy Best First Search Algorithm
 def GBFS(startNode, heuristics, graph, goalNode="Bucharest"):
+    ''' Greedy Best First Search Algorithm '''
     priorityQueue = queue.PriorityQueue()
     priorityQueue.put((heuristics[startNode], startNode))
 
@@ -90,13 +118,13 @@ def GBFS(startNode, heuristics, graph, goalNode="Bucharest"):
     return path
 
 
-# Astar Algorithm
 def Astar(startNode, heuristics, graph, goalNode="Bucharest"):
+    ''' Astar Algorithm '''
     priorityQueue = queue.PriorityQueue()
     distance = 0
-    path = []
-
     priorityQueue.put((heuristics[startNode] + distance, [startNode, 0]))
+
+    path = []
 
     while priorityQueue.empty() == False:
         current = priorityQueue.get()[1]
@@ -116,7 +144,7 @@ def Astar(startNode, heuristics, graph, goalNode="Bucharest"):
 
 
 # drawing map of answer
-def drawMap(city, gbfs, astar, graph):
+def drawMap(city, graph, *paths):
     for i, j in city.items():
         plt.plot(j[0], j[1], "ro")
         plt.annotate(i, (j[0] + 5, j[1]))
@@ -125,26 +153,19 @@ def drawMap(city, gbfs, astar, graph):
             n = city[k[0]]
             plt.plot([j[0], n[0]], [j[1], n[1]], "gray")
 
-    for i in range(len(gbfs)):
-        try:
-            first = city[gbfs[i]]
-            secend = city[gbfs[i + 1]]
+    color = ["red", "green", "blue", "orange", "purple", "yellow", "cyan"]
 
-            plt.plot([first[0], secend[0]], [first[1], secend[1]], "green")
-        except:
-            continue
+    for path in paths:
+        for i in range(len(path)):
+            try:
+                first = city[path[i]]
+                secend = city[path[i + 1]]
 
-    for i in range(len(astar)):
-        try:
-            first = city[astar[i]]
-            secend = city[astar[i + 1]]
+                plt.plot([first[0], secend[0]], [first[1], secend[1]], color[paths.index(path)])
+            except:
+                continue
 
-            plt.plot([first[0], secend[0]], [first[1], secend[1]], "blue")
-        except:
-            continue
-
-    plt.errorbar(1, 1, label="GBFS", color="green")
-    plt.errorbar(1, 1, label="ASTAR", color="blue")
+        plt.errorbar(1, 1, label=f"{paths.index(path)}", color=color[paths.index(path)])
     plt.legend(loc="lower left")
 
     plt.show()
@@ -152,9 +173,9 @@ def drawMap(city, gbfs, astar, graph):
 
 # running the program
 def main():
-    heuristic = getHeuristics()
-    graph = createGraph()
-    city, citiesCode = getCity()
+    heuristic = DataLoader.getHeuristics()
+    graph = DataLoader.createGraph()
+    city, citiesCode = DataLoader.getCity()
 
     for i, j in citiesCode.items():
         print(i, j)
@@ -167,12 +188,13 @@ def main():
 
         cityName = citiesCode[inputCode]
 
+        ucs = UCS(cityName, graph)
         gbfs = GBFS(cityName, heuristic, graph)
         astar = Astar(cityName, heuristic, graph)
         print("GBFS => ", gbfs)
         print("ASTAR => ", astar)
-
-        drawMap(city, gbfs, astar, graph)
+        print("UCS => ", ucs)
+        drawMap(city, graph, gbfs, astar, ucs)
 
 
 main()
